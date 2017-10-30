461954f9d92c1e3a234c488aef09b49299f46d66
Vitaliy Abramov <i@kassad.ru>
2017-10-30 21:20:40 +0500

    init

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { Path, Point, view, setup } from "paper";
+
+const begin = () => {
+
+	const canvas: HTMLCanvasElement =
+		document.getElementById("myCanvas") as HTMLCanvasElement
+
+	setup(canvas)
+
+	const path = new Path()
+	path.strokeColor = "black"
+
+	const start = new Point(100, 100)
+	path.moveTo(start)
+	path.lineTo(start.add([200, -50]))
+	view.draw()
+
+}
+
+window.onload = begin
+
+
